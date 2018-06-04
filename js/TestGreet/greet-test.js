f5525bab0e55019606e19727b99caed606659f13
ludwej <jacobsludwe84@gmail.com>
2018-06-04 16:51:35 +0200

    tests

--- js/TestGreet/greet-test.js
+++ js/TestGreet/greet-test.js
@@ -15,10 +15,24 @@ describe('Greeting', function(){
 
 
 
-      it('should greet Lihle in Afrikaans', function(){
+      it('should count how many people greeted', function(){
         var factoryF = GreetF()
 
         factoryF.greetFunction("Lihle",'Afri');
-        assert.equal(factoryF.greetFunction('Halo, Lihle'))
+        factoryF.greetFunction("Ludwe",'Xho');
+        factoryF.greetFunction("Aphiwe",'Eng');
+
+        assert.equal(factoryF.countLocal() , 3 )
+        });
+
+        it('should count how many people greeted', function(){
+          var factoryF = GreetF()
+
+          factoryF.greetFunction("Ludwe",'Afri');
+          factoryF.greetFunction("ludwe",'Xho');
+          factoryF.greetFunction("Ludwe",'Eng');
+
+
+          assert.equal(factoryF.countLocal(1))
         });
 });
